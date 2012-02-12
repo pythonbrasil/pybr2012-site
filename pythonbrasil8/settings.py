a736d520fdb3ab1cdc6eb061e95d416c7c434718
mayza de oliveira <mayza.deoliveira@gmail.com>
2012-02-12 20:56:39 -0200

    adding PROJECT_DIR for settings

--- pythonbrasil8/settings.py
+++ pythonbrasil8/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -75,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'static_files'),
 )
 
 # List of finder classes that know how to find static files in
