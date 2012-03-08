1f883f8f490fa672e5593cba190015ee991624c5
Flavia Missi <flaviamissi@gmail.com>
2012-03-08 14:52:42 -0300

    Added sponsor listing in home template

--- pythonbrasil8/core/views.py
+++ pythonbrasil8/core/views.py
@@ -1,6 +1,14 @@
 from django.views.generic import ListView
+from mittun.sponsors.models import Sponsor
+
 from core.models import Home
+
 
 class Home(ListView):
     model = Home
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['sponsors'] = Sponsor.objects.all()
+        return context
