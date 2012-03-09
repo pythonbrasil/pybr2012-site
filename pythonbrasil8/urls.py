f770b9195fbdacee15b877b7d38d726dfeabc108
Francisco Souza <f@souza.cc>
2012-03-08 22:18:14 -0300

    pre-registration: added pre-registration form
     pending: successful page :)

--- pythonbrasil8/urls.py
+++ pythonbrasil8/urls.py
@@ -1,12 +1,16 @@
-from django.conf.urls.defaults import patterns, url, include
+# -*- coding: utf-8 -*-
 from django.conf import settings
+from django.conf.urls.defaults import patterns, url, include
+from mittun.registration.views import SubscribeView
+
 from core.views import Home
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$',  Home.as_view(), name='home'),
+    url(r'^$', Home.as_view(), name='home'),
+    url(r'^pre-registration/$', SubscribeView.as_view(), name='pre-registration'),
     url(r'^mittun/', include('mittun.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
