822b0d9579cf48f6b7f3526f0e30d77d1f00fb8b
Andrews Medina <andrewsmedina@gmail.com>
2012-04-25 00:09:15 -0300

    only authenticated users should access dashboard

--- pythonbrasil8/dashboard/tests.py
+++ pythonbrasil8/dashboard/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
 from django.views.generic import TemplateView
+from django.contrib.auth.models import User
 
 from views import IndexView
 
@@ -11,3 +13,17 @@ class DashboardIndexTestCase(TestCase):
 
     def test_shoud_use_a_dashboard_template(self):
         self.assertEqual('dashboard/index.html', IndexView.template_name)
+
+    def test_should_redirects_if_user_is_not_logged_in(self):
+        request = RequestFactory().get("/")
+        user = User()
+        user.is_authenticated = lambda : False
+        request.user = user
+        result = IndexView.as_view()(request)
+        self.assertEqual(302, result.status_code)
+
+    def test_should_have_200_status_code_when_user_is_logged_in(self):
+        request = RequestFactory().get("/")
+        request.user = User()
+        result = IndexView.as_view()(request)
+        self.assertEqual(200, result.status_code)
