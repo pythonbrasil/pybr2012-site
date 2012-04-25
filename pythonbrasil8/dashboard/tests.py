0f0a1797dd42079895ff164b83dcd1c05e86fa54
Andrews Medina <andrewsmedina@gmail.com>
2012-04-24 23:18:24 -0300

    initial dashboar index view

--- pythonbrasil8/dashboard/tests.py
+++ pythonbrasil8/dashboard/tests.py
@@ -1,16 +1,13 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.views.generic import TemplateView
+
+from views import IndexView
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class DashboardIndexTestCase(TestCase):
+
+    def test_should_be_a_template_view(self):
+        self.assertTrue(issubclass(IndexView, TemplateView))
+
+    def test_shoud_use_a_dashboard_template(self):
+        self.assertEqual('dashboard/index.html', IndexView.template_name)
--- pythonbrasil8/dashboard/views.py
+++ pythonbrasil8/dashboard/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'dashboard/index.html'
