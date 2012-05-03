b2d65c59c55bb75bd94f30937fd2d53a3f4df5f2
Andrews Medina <andrewsmedina@gmail.com>
2012-05-03 00:26:48 -0300

    added urls for subscribe session view

--- pythonbrasil8/dashboard/urls.py
+++ pythonbrasil8/dashboard/urls.py
@@ -5,4 +5,5 @@ from views import IndexView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='dashboard-index'),
+    url(r'^subscribe/$', 'pythonbrasil8.schedule.views.session_subscribe_view', name='session-subscribe'),
 )
