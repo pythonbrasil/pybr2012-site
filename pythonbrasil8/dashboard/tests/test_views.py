b0ad411726b927415844e63af42f7b6d6cad218e
Andrews Medina <andrewsmedina@gmail.com>
2012-05-03 01:11:40 -0300

    added session on context for dashboard view

--- pythonbrasil8/dashboard/tests/test_views.py
+++ pythonbrasil8/dashboard/tests/test_views.py
@@ -4,9 +4,21 @@ from django.views.generic import TemplateView
 from django.contrib.auth.models import User
 
 from pythonbrasil8.dashboard.views import IndexView
+from pythonbrasil8.schedule.models import Session
 
 
 class DashboardIndexTestCase(TestCase):
+
+    def setUp(self):
+        self.request = RequestFactory().get("/")
+        self.request.user = User.objects.create(username="user")
+        session = Session.objects.create(
+            title="Python for dummies",
+            description="about python, universe and everything",
+            type="talk",
+            tags="python, 42",
+        )
+        session.speakers.add(self.request.user)
 
     def test_should_be_a_template_view(self):
         self.assertTrue(issubclass(IndexView, TemplateView))
@@ -15,15 +27,15 @@ class DashboardIndexTestCase(TestCase):
         self.assertEqual('dashboard/index.html', IndexView.template_name)
 
     def test_should_redirects_if_user_is_not_logged_in(self):
-        request = RequestFactory().get("/")
-        user = User()
-        user.is_authenticated = lambda : False
-        request.user = user
-        result = IndexView.as_view()(request)
+        self.request.user.is_authenticated = lambda : False
+        result = IndexView.as_view()(self.request)
         self.assertEqual(302, result.status_code)
 
     def test_should_have_200_status_code_when_user_is_logged_in(self):
-        request = RequestFactory().get("/")
-        request.user = User()
-        result = IndexView.as_view()(request)
+        result = IndexView.as_view()(self.request)
         self.assertEqual(200, result.status_code)
+
+    def test_should_have_sessions_on_context(self):
+        result = IndexView.as_view()(self.request)
+        self.assertIn('sessions', result.context_data)
+        self.assertQuerysetEqual(result.context_data['sessions'], [u"Python for dummies",], lambda s: s.title)
--- pythonbrasil8/dashboard/views.py
+++ pythonbrasil8/dashboard/views.py
@@ -3,9 +3,16 @@ from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
+from pythonbrasil8.schedule.models import Session
+
 
 class IndexView(TemplateView):
     template_name = 'dashboard/index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndexView, self).get_context_data(*args, **kwargs)
+        context['sessions'] = Session.objects.filter(speakers=self.request.user)
+        return context
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
