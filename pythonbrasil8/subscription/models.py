70f2f11dbd1f4cbc825926e301729bb69a1fd68e
Andrews Medina <andrewsmedina@gmail.com>
2012-05-14 22:19:11 -0300

    model for Subscription

--- pythonbrasil8/subscription/models.py
+++ pythonbrasil8/subscription/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subscription(models.Model):
+
+    TYPE = (
+        ('tutorial', 'tutorial',),
+        ('talk', 'talk'),
+    )
+
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    type = models.CharField(max_length=25, choices=TYPE)
--- pythonbrasil8/subscription/tests.py
+++ pythonbrasil8/subscription/tests.py
@@ -1,16 +1,40 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.db import models
+from django.contrib.auth.models import User
+
+from pythonbrasil8.subscription.models import Subscription
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class SubscriptionModelTestCase(TestCase):
+
+    def test_should_have_type(self):
+        self.assert_field_in('type', Subscription)
+
+    def test_type_should_be_CharField(self):
+        type_field = Subscription._meta.get_field_by_name('type')[0]
+        self.assertIsInstance(type_field, models.CharField)
+
+    def test_type_should_have_choices(self):
+        type_field = Subscription._meta.get_field_by_name('type')[0]
+        choices = [choice[0] for choice in type_field._choices]
+        self.assertIn('talk', choices)
+        self.assertIn('tutorial', choices)
+
+    def test_should_have_user(self):
+        self.assert_field_in('user', Subscription)
+
+    def test_user_should_be_a_foreign_key(self):
+        user_field = Subscription._meta.get_field_by_name('user')[0]
+        self.assertIsInstance(user_field, models.ForeignKey)
+        self.assertEqual(User, user_field.related.parent_model)
+
+    def test_should_have_date(self):
+        self.assert_field_in('date', Subscription)
+
+    def test_date_should_be_datetime_field(self):
+        date_field = Subscription._meta.get_field_by_name('date')[0]
+        self.assertIsInstance(date_field, models.DateTimeField)
+        self.assertTrue(date_field.auto_now_add)
+
+    def assert_field_in(self, field_name, model):
+        self.assertIn(field_name, model._meta.get_all_field_names())
