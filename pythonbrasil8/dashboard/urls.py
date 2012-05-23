e82b3cc8c28db537bd14a7f270f91525399d917b
Andrews Medina <andrewsmedina@gmail.com>
2012-05-23 00:37:36 -0300

    urls for talk subscription

--- pythonbrasil8/dashboard/urls.py
+++ pythonbrasil8/dashboard/urls.py
@@ -2,9 +2,12 @@
 from django.conf.urls import patterns, url
 
 from pythonbrasil8.dashboard.views import IndexView, ProfileView
+from pythonbrasil8.subscription.views import SubscriptionView
+
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='dashboard-index'),
     url(r'^subscribe/$', 'pythonbrasil8.schedule.views.session_subscribe_view', name='session-subscribe'),
+    url(r'^subscription/talk/$', SubscriptionView.as_view(), name='talk-subscription'),
     url(r'^profile/$', ProfileView.as_view(), name="edit-profile")
 )
