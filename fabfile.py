4910f97cbb69968a1957d10f019df0807dd05c34
Francisco Souza <f@souza.cc>
2012-06-04 03:04:43 -0300

    fabfile: removed app_root env var
    This var is not necessary anymore with django 1.4.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,7 @@
 from fabric.api import cd, env, run
 
-import os
-
 
 env.project_root = '/home/pythonbrasil/pythonbrasil8'
-env.app_root = os.path.join(env.project_root, 'pythonbrasil8')
 env.virtualenv = '/home/pythonbrasil/env'
 env.hosts = ['2012.pythonbrasil.org.br']
 
@@ -15,7 +12,7 @@ def update_app(tag):
 
 
 def collect_static_files():
-    with cd(env.app_root):
+    with cd(env.project_root):
         run("%(virtualenv)s/bin/python manage.py compress" % env)
         run("%(virtualenv)s/bin/python manage.py collectstatic -v 0 --noinput" % env)
 
@@ -25,31 +22,31 @@ def pip_install():
 
 
 def start():
-    with cd(env.app_root):
+    with cd(env.project_root):
         run('DJANGO_SETTINGS_MODULE=pythonbrasil8.settings_local %(virtualenv)s/bin/gunicorn --pid=gunicorn.pid --bind=127.0.0.1:8080 --daemon --workers=3 pythonbrasil8.wsgi:application' % env)
 
 
 def reload():
-    run('kill -HUP `cat %(app_root)s/gunicorn.pid`' % env)
+    run('kill -HUP `cat %(project_root)s/gunicorn.pid`' % env)
 
 
 def stop():
-    run('kill -9 `cat %(app_root)s/gunicorn.pid`' % env)
+    run('kill -KILL `cat %(project_root)s/gunicorn.pid`' % env)
 
 
 def syncdb():
-    with cd(env.app_root):
+    with cd(env.project_root):
         run("%(virtualenv)s/bin/python manage.py syncdb --noinput" % env)
         run("%(virtualenv)s/bin/python manage.py migrate --noinput" % env)
 
 
 def loaddata():
-    with cd(env.app_root):
+    with cd(env.project_root):
         run("%s/bin/python manage.py loaddata fixtures/initial_data.json" % env.virtualenv)
 
 
 def limpar_pycs():
-    with cd(env.app_root):
+    with cd(env.project_root):
         run("find . -name \"*.pyc\" | xargs rm -f ")
 
 
