a80d6553babf737045d2aeee97090fe997230509
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-06-23 18:34:06 -0300

    Improving reset password

--- pythonbrasil8/urls.py
+++ pythonbrasil8/urls.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
-from django.contrib.auth.views import password_reset
+from django.contrib.auth.views import password_reset, password_reset_confirm
 from django.views.generic import TemplateView
 from registration.forms import RegistrationForm
 
@@ -29,10 +29,13 @@ urlpatterns = patterns('',
     url(r'^notification/$', NotificationView.as_view(), name='notification'),
 
     url(r'^dashboard/', include('pythonbrasil8.dashboard.urls')),
+
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'extra_context': {'registration_form': RegistrationForm()}}, name='auth_login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {"next_page": "/"}, name='auth_logout'),
     url(r'^accounts/password/reset/$', password_reset, {'email_template_name': 'email_password_reset.txt',  'subject_template_name': 'email_password_reset_title.txt', 'template_name': 'password_reset.html',}, name='password_reset'),
     url(r'^accounts/password/reset/done/$', TemplateView.as_view(template_name="password_reset_sent.html"), name='password_reset_sent'),
+    url(r'^accounts/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, name='password_reset_confirm'),
     url(r'^accounts/', include('registration.backends.default.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
