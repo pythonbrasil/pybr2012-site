4a49cff3aa50757201866e9d196f12ec7643073c
Francisco Souza <f@souza.cc>
2012-06-23 15:26:55 -0300

    schedule: added Track to the admin

--- pythonbrasil8/schedule/admin.py
+++ pythonbrasil8/schedule/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from pythonbrasil8.schedule.models import Session
+from pythonbrasil8.schedule.models import Session, Track
 
 
 class SessionAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'type')
     list_filter = ('type',)
 
-
 admin.site.register(Session, SessionAdmin)
+
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Track, TrackAdmin)
