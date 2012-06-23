0f35577c051d5cd64746fef36a77903004464322
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2012-06-23 15:44:26 -0300

    Password reset

--- pythonbrasil8/urls.py
+++ pythonbrasil8/urls.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
+from django.contrib.auth.views import password_reset
 from django.views.generic import TemplateView
 from registration.forms import RegistrationForm
 
@@ -30,6 +31,7 @@ urlpatterns = patterns('',
     url(r'^dashboard/', include('pythonbrasil8.dashboard.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'extra_context': {'registration_form': RegistrationForm()}}, name='auth_login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {"next_page": "/"}, name='auth_logout'),
+    url(r'^accounts/password/reset/$', password_reset, {'template_name': 'password_reset.html'}),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
