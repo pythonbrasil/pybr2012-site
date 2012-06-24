26e3fcba493213bb78c842424f2ee4c964625a10
Francisco Souza <f@souza.cc>
2012-06-24 12:30:41 -0300

    schedule: success message
    The message tells the user that its message has been successfully
    submitted.

--- pythonbrasil8/schedule/views.py
+++ pythonbrasil8/schedule/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import CreateView
+from django import http
+from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
+from django.views.generic import CreateView
 
-from pythonbrasil8.schedule.forms import SessionForm
 from pythonbrasil8.core.views import LoginRequiredMixin
+from pythonbrasil8.schedule.forms import SessionForm
 
 
 class SubscribeView(LoginRequiredMixin, CreateView):
@@ -12,3 +15,9 @@ class SubscribeView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse('dashboard-index')
+
+    def post(self, request, *args, **kwargs):
+        r = super(SubscribeView, self).post(request, *args, **kwargs)
+        if isinstance(r, http.HttpResponseRedirect):
+            messages.success(request, _("Session successfully submitted!"), fail_silently=True)
+        return r
