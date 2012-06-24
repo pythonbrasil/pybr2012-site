2acbd8c7baf9315c6f9dc177df0c5337529f9ab8
Francisco Souza <f@souza.cc>
2012-06-24 17:26:05 -0300

    schedule: improving admin
    Including status, track and the name of the speakers to the list of
    sessions, also allowing to filtering by status.
    
    Removed description from list_display, because it does not make any
    sense to display the description in a table.

--- pythonbrasil8/schedule/admin.py
+++ pythonbrasil8/schedule/admin.py
@@ -1,16 +1,30 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from pythonbrasil8.schedule.models import Session, Track
 
 
+def coalesce(*args):
+    for arg in args:
+        if arg:
+            return arg
+
+
 class SessionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'type')
-    list_filter = ('type',)
+    list_display = ("title", "type", "speaker_names", "track", "status",)
+    list_filter = ("type", "status",)
+
+    def speaker_names(self, obj):
+        speakers = [coalesce(s.accountprofile.name, s.username) for s in obj.speakers.all().order_by("username")]
+        return ", ".join(speakers)
+
+    speaker_names.short_description = _("Speakers")
 
 admin.site.register(Session, SessionAdmin)
 
 
 class TrackAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ("name",)
 
 admin.site.register(Track, TrackAdmin)
