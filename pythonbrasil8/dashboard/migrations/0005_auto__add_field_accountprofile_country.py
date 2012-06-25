b4a7f240bfe2a3398e42a2b4fde7e8fb42a8d5f0
Francisco Souza <f@souza.cc>
2012-06-24 22:28:31 -0300

    dashboard: pep8 fix

--- pythonbrasil8/dashboard/migrations/0005_auto__add_field_accountprofile_country.py
+++ pythonbrasil8/dashboard/migrations/0005_auto__add_field_accountprofile_country.py
@@ -1,22 +1,15 @@
 # encoding: utf-8
-import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
+
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
-        # Adding field 'AccountProfile.country'
         db.add_column('dashboard_accountprofile', 'country', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True), keep_default=False)
 
-
     def backwards(self, orm):
-        
-        # Deleting field 'AccountProfile.country'
         db.delete_column('dashboard_accountprofile', 'country')
-
 
     models = {
         'auth.group': {
