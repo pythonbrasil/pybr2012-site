f85017a20a485954328a6537d78085d005232a6f
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-10-04 08:37:13 -0300

    Add slug to Track and Session and its migrations

--- pythonbrasil8/schedule/models.py
+++ pythonbrasil8/schedule/models.py
@@ -1,22 +1,11 @@
 # -*- coding: utf-8 -*-
-import transmeta
 
+import transmeta
 from django.contrib.auth import models as auth_models
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.template.defaultfilters import slugify
 
-
-class Track(models.Model):
-    __metaclass__ = transmeta.TransMeta
-
-    name = models.CharField(max_length=255, verbose_name=_("Name"))
-    description = models.CharField(max_length=2000, verbose_name=_("Description"))
-
-    class Meta:
-        translate = ("name", "description")
-
-    def __unicode__(self):
-        return self.name
 
 SESSION_TYPES = (
     ("talk", _("Talk")),
@@ -42,13 +31,42 @@ SESSION_LEVELS = (
     (u"advanced",  _(u"Advanced")),
 )
 
+class Track(models.Model):
+    __metaclass__ = transmeta.TransMeta
+
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+    slug = models.SlugField(max_length=255, default=None, null=True,
+            blank=True)
+    description = models.CharField(max_length=2000,
+            verbose_name=_("Description"))
+
+    class Meta:
+        translate = ("name", "description")
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Track, self).save(*args, **kwargs)
 
 class Session(models.Model):
-    type = models.CharField(max_length=20, choices=SESSION_TYPES, verbose_name=_("Type"))
+    type = models.CharField(max_length=20, choices=SESSION_TYPES,
+            verbose_name=_("Type"))
     track = models.ForeignKey(Track, verbose_name=_("Track"))
-    audience_level =  models.CharField(max_length=12, choices=SESSION_LEVELS, verbose_name=_("Audience level"))
-    language = models.CharField(max_length=2, verbose_name=_("Language"), choices=LANGUAGE_CHOICES)
+    audience_level =  models.CharField(max_length=12, choices=SESSION_LEVELS,
+            verbose_name=_("Audience level"))
+    language = models.CharField(max_length=2, verbose_name=_("Language"),
+            choices=LANGUAGE_CHOICES)
     title = models.CharField(max_length=255, verbose_name=_("Title"))
+    slug = models.SlugField(max_length=255, default=None, null=True,
+            blank=True)
     description = models.TextField(verbose_name=_("Description"))
-    speakers = models.ManyToManyField(auth_models.User, verbose_name=_("Speakers"))
-    status = models.CharField(max_length=10, choices=SESSION_STATUSES, default="proposed")
+    speakers = models.ManyToManyField(auth_models.User,
+            verbose_name=_("Speakers"))
+    status = models.CharField(max_length=10, choices=SESSION_STATUSES,
+            default="proposed")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Session, self).save(*args, **kwargs)
