8ac20045d5d31854a7ea815cee869ed148442ea8
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-10-04 09:26:48 -0300

    Change proposal page URL

--- pythonbrasil8/schedule/views.py
+++ pythonbrasil8/schedule/views.py
@@ -89,12 +89,9 @@ def track_page(request, track_slug):
     return shortcuts.render_to_response('track.html', data,
             context_instance=RequestContext(request))
 
-def proposal_page(request, proposal_id, proposal_slug=''):
-    proposal = shortcuts.get_object_or_404(Session, id=proposal_id)
-    slug = slugify(proposal.title)
-    if not proposal_slug or not proposal_slug == slug:
-        return http.HttpResponseRedirect(reverse('proposal-page',
-                kwargs={'proposal_id': proposal_id, 'proposal_slug': slug}))
+def proposal_page(request, track_slug, proposal_slug):
+    track = shortcuts.get_object_or_404(Track, slug=track_slug)
+    proposal = shortcuts.get_object_or_404(Session, slug=proposal_slug)
 
     speakers = []
     for speaker in proposal.speakers.all():
