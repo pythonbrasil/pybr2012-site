ed5b774aa8ddf1720d8176d282fa20a700e0bbf3
Eduardo Stalinho <eduardooc.86@gmail.com>
2012-11-03 01:15:00 -0200

    removes comment line

--- pythonbrasil8/news/urls.py
+++ pythonbrasil8/news/urls.py
@@ -6,5 +6,4 @@ from pythonbrasil8.news.models import Post
 
 urlpatterns = patterns('pythonbrasil8.news.views',
     url(r'^$', 'news_view', name='main'),
-    #url(r'^posts/(?P<post_slug>[\w\d-]+)/$', ProductView.as_view(), name='post'),
 )
