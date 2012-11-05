c30994f7d49973f0e6706ffd66b06be4c792c787
Eduardo Stalinho <eduardooc.86@gmail.com>
2012-11-04 23:01:12 -0200

    add url for detailed post and for rss feed

--- pythonbrasil8/news/urls.py
+++ pythonbrasil8/news/urls.py
@@ -2,8 +2,10 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-from pythonbrasil8.news.models import Post
+from pythonbrasil8.news.feed import NewsFeed
 
 urlpatterns = patterns('pythonbrasil8.news.views',
     url(r'^$', 'news_view', name='main'),
+    url(r'^feed/$', NewsFeed(), name='feed'),
+    url(r'^(?P<post_slug>[\w\d-]+)/$', 'post_view', name='post'),
 )
